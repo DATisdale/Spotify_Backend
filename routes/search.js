81e819d015a87dbce45819fdb92e0a7eb26214ac
Darrell Tisdale <Darrell.Tisdale@outlook.com>
2022-03-22 22:03:21 -0400

    all routes are working

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { Song } = require("../models/song");
-const { Playlist } = require("../models/playList");
+const { PlayList } = require("../models/playList");
 const auth = require("../middleware/auth");
 
 router.get("/", auth, async (req, res) => {
@@ -9,7 +9,7 @@ router.get("/", auth, async (req, res) => {
     const songs = await Song.find({
       name: { $regex: search, $options: "i" },
     }).limit(10);
-    const playlists = await Playlist.find({
+    const playlists = await PlayList.find({
       name: { $regex: search, $options: "i" },
     }).limit(10);
     const result = { songs, playlists };
